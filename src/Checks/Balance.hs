1e7e1552bd76ae647bb0f9872490ced236556dd9
Philipp Kant <philipp.kant@iohk.io>
2017-09-23 12:57:12 +0200

    Check for the correct amount of Lovelace in each account

--- src/Checks/Balance.hs
+++ src/Checks/Balance.hs
@@ -3,6 +3,9 @@
 module Checks.Balance
     (balanceChecks) where
 
+import Data.List
+import Data.Ord
+import Data.Int (Int64)
 import Control.Monad.Reader
 import Checks.Types
 import Data.Aeson as JSON
@@ -46,27 +49,30 @@ doCheckAvvmDistribution GenesisData{..} = do
     JSON.eitherDecode . toS <$> liftIO (T.readFile avvmFile) >>= \case
         Left err -> pure $ CheckFailed $ "Could not parse AvvmBalance: " <> err
         Right ledger -> do
-            let avvmHashmap = HM.fromList $ (\(AvvmEntry address ada) -> (convertToBS64URL address, toS . show $ 1000000 * ada)) <$> ledger
-            pure $ if avvmHashmap == gdAvvmDistr
+            let avvmHashmap = HM.fromList $ (\(AvvmEntry address ada) -> (convertToBS64URL address, fromIntegral $ 1000000 * ada :: Int64)) <$> ledger
+                genesisHashmap = HM.map (read . toS) gdAvvmDistr
+                imbalance = HM.filter (/= 0) $ HM.intersectionWith (-) avvmHashmap genesisHashmap
+            pure $ if avvmHashmap == genesisHashmap && HM.null imbalance
                 then CheckPassed
-                else let missing = avvmHashmap `HM.difference` gdAvvmDistr
-                         unexpected = gdAvvmDistr `HM.difference` avvmHashmap
+                else let missing = avvmHashmap `HM.difference` genesisHashmap
+                         unexpected = genesisHashmap `HM.difference` avvmHashmap
                          nMissing = HM.size missing
                          nUnexpected = HM.size unexpected
+                         nImbalance = HM.size imbalance
 
                      in CheckFailed $ unlines [ "Mismatch in Avvm balance"
-                                              , show nMissing <> " Addresses missing in the genesis data: "
-                                                <> if verbose
-                                                  then toS . PP.pShowNoColor. HM.toList $ missing
-                                                  else (toS . PP.pShowNoColor . take 5 . HM.toList $ missing)
-                                                       <> "\n    and " <> show (nMissing - 5) <> " more"
-                                              , show nUnexpected <> " Unexpected addresses: "
-                                                <> if verbose
-                                                   then toS . PP.pShowNoColor . HM.toList $ unexpected
-                                                   else (toS . PP.pShowNoColor . take 2 . HM.toList $ unexpected)
-                                                        <> "\n    and " <> show (nUnexpected - 5) <> " more"
+                                              , show nMissing <> " address(es) missing in the genesis data: " <> showHashmap verbose missing
+                                              , show nUnexpected <> " unexpected address(es): " <> showHashmap verbose unexpected
+                                              , show nImbalance <> " account(s) do not contain the right amount of Lovelace: " <> showHashmap verbose imbalance
                                               ]
-                        -- TODO: look for imbalances in addresses that exist in both
+  where
+    showOrderedHashmap = toS . PP.pShowNoColor . reverse . sortBy (comparing snd) . HM.toList
+    showTruncatedHashmap hm = (toS . PP.pShowNoColor . take 5 . HM.toList $ hm)
+                              <> "\n    and " <> show (HM.size hm - 5) <> " more"
+    showHashmap verbosity hm =
+        if verbosity || HM.size hm <= 5
+        then showOrderedHashmap hm
+        else showTruncatedHashmap hm
 
 convertToBS64URL :: Text -> Text
 convertToBS64URL = toS
